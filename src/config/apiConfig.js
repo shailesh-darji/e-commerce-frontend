bea95144a0cd9e73952e0b30d8adece7c26656b4
Shailesh <shailesh.d@simformsolutions.com>
2023-10-23 11:53:50 +0530

    Change in api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "https://e-commerce-backend-production-e38c.up.railway.app"
 const jwt = localStorage.getItem("jwt")
 
 export const api=axios.create({
